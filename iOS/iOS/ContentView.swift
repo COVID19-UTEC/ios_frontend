33c394c490767abe5fcaae638b58c783417d6a13
Andrea Diaz <andrea.diaz.gardini@gmail.com>
2020-03-21 20:27:49 -0500

    App first set up

---
+++ iOS/iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS
+//
+//  Created by Andrea Diaz ♡ on 3/21/20.
+//  Copyright © 2020 COVID19-UTEC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
